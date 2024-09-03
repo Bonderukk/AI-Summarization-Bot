cd8f9969e03ae119ac8dcceb4f140a7156dc8121
Matúš Bednařík <matusmatonibednarik@gmail.com>
2024-09-03 16:42:11 +0200

    tst

--- XBot.py
+++ XBot.py
@@ -75,5 +75,5 @@ if __name__ == "__main__":
         print(f"URL: {link}\n")
         print("Summary:\n")
         print(result)
-        print("\n" + "=" * 80 + "\n")
+        print("\n" + "=" * 80 + "\nswag")
 
